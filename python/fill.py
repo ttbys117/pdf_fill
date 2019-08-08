648bb2ec451b68517a7cf6fe87a48b35c56b6313
ttbys117 <skimura@jp.fujitsu.com>
2019-08-08 14:53:29 +0900

    Initial commit

---
+++ python/fill.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+
+import os
+import pdfrw
+
+
+INVOICE_TEMPLATE_PATH = '../formats/master_form.pdf'
+INVOICE_OUTPUT_PATH = '../dest/filled_form_py.pdf'
+
+
+def write_fillable_pdf(input_pdf_path, output_pdf_path, data_dict):
+
+    ANNOT_KEY = '/Annots'
+    ANNOT_FIELD_KEY = '/T'
+#    ANNOT_VAL_KEY = '/V'
+#    ANNOT_RECT_KEY = '/Rect'
+    SUBTYPE_KEY = '/Subtype'
+    WIDGET_SUBTYPE_KEY = '/Widget'
+
+    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    annotations = template_pdf.pages[0][ANNOT_KEY]
+    for annotation in annotations:
+        print(repr(annotation))
+        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+            if annotation[ANNOT_FIELD_KEY]:
+                key = annotation[ANNOT_FIELD_KEY][1:-1]
+                if key in data_dict.keys():
+                    annotation.update(
+                        pdfrw.PdfDict(V='{}'.format(data_dict[key]))
+                    )
+                    print('{} -> {}.'.format(key, data_dict[key]))
+    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+
+
+data_dict = {
+        'firstName': '太郎',
+        'lastName': 'ＰＤＦ',
+        'address': 'Python市　１－２－３'
+}
+
+if __name__ == '__main__':
+    if os.path.exists(INVOICE_OUTPUT_PATH):
+        os.remove(INVOICE_OUTPUT_PATH)
+    write_fillable_pdf(INVOICE_TEMPLATE_PATH, INVOICE_OUTPUT_PATH, data_dict)
